926a9bb26f1056efb59508b82d94d7fa62fa2045
ralegria77 <ralegria77@gmail.com>
2020-10-28 15:48:39 -0700

    commit tree 6894c9ee1b36bee614467b4f7e4a85985745d53d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+require 'pry'
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+# ["Krae", "Bob", "David"]
+def batch_badge_creator(people) 
+    #new_array = []
+    people.collect do |i|
+        "Hello, my name is #{i}." 
+    end
+    #return new_array
+end
+
+def assign_rooms(attendees)
+    attendees.collect do |vlorg|
+        "Hello, #{vlorg}! You'll be assigned to room #{attendees.index(vlorg)+1}!"
+    end
+
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |babs|
+        puts babs
+    end
+    assign_rooms(attendees).each do |cabs|
+        puts cabs
+    end
+
+end
+
+
+
+
+
